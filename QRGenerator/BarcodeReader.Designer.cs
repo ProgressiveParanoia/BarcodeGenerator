ccf58a895015d26491099989917f5dfaf80186e0
ProgressiveParanoia <sniperneroro@twitch.tv>
2022-08-03 02:47:43 +0800

    - Basic property tab - Basic selection - Save file dialog

--- QRGenerator/BarcodeReader.Designer.cs
+++ QRGenerator/BarcodeReader.Designer.cs
@@ -32,6 +32,7 @@ namespace QRGenerator
             this.mainLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.barcodePicBox = new System.Windows.Forms.PictureBox();
             this.itemListLayout = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnSave = new System.Windows.Forms.Button();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.lblBarCode = new System.Windows.Forms.Label();
             this.barcodeTxtBox = new System.Windows.Forms.TextBox();
@@ -40,12 +41,18 @@ namespace QRGenerator
             this.extraTxtBox = new System.Windows.Forms.TextBox();
             this.btnPickFont = new System.Windows.Forms.Button();
             this.btnExtraAdd = new System.Windows.Forms.Button();
+            this.btnBarcodeFont = new System.Windows.Forms.Button();
+            this.propertiesLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.extraTextPropertyTxtBox = new System.Windows.Forms.TextBox();
+            this.btnPropertyChangeFont = new System.Windows.Forms.Button();
             this.fontPicker = new System.Windows.Forms.FontDialog();
-            this.btnSave = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
             this.mainLayoutPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.barcodePicBox)).BeginInit();
             this.itemListLayout.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
+            this.propertiesLayoutPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainLayoutPanel
@@ -57,6 +64,7 @@ namespace QRGenerator
             this.mainLayoutPanel.Controls.Add(this.barcodePicBox, 1, 0);
             this.mainLayoutPanel.Controls.Add(this.itemListLayout, 1, 1);
             this.mainLayoutPanel.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.mainLayoutPanel.Controls.Add(this.propertiesLayoutPanel, 2, 0);
             this.mainLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mainLayoutPanel.Location = new System.Drawing.Point(0, 0);
             this.mainLayoutPanel.Name = "mainLayoutPanel";
@@ -86,6 +94,17 @@ namespace QRGenerator
             this.itemListLayout.Size = new System.Drawing.Size(897, 97);
             this.itemListLayout.TabIndex = 3;
             // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(3, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(894, 94);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.Controls.Add(this.lblBarCode);
@@ -95,7 +114,9 @@ namespace QRGenerator
             this.flowLayoutPanel1.Controls.Add(this.extraTxtBox);
             this.flowLayoutPanel1.Controls.Add(this.btnPickFont);
             this.flowLayoutPanel1.Controls.Add(this.btnExtraAdd);
+            this.flowLayoutPanel1.Controls.Add(this.btnBarcodeFont);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(127, 639);
@@ -109,7 +130,7 @@ namespace QRGenerator
             this.lblBarCode.Name = "lblBarCode";
             this.lblBarCode.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
             this.lblBarCode.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.lblBarCode.Size = new System.Drawing.Size(91, 13);
+            this.lblBarCode.Size = new System.Drawing.Size(120, 13);
             this.lblBarCode.TabIndex = 0;
             this.lblBarCode.Text = "Barcode Text";
             this.lblBarCode.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -139,7 +160,7 @@ namespace QRGenerator
             this.label1.Name = "label1";
             this.label1.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
             this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.Size = new System.Drawing.Size(120, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Extra Text";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -167,20 +188,61 @@ namespace QRGenerator
             this.btnExtraAdd.Name = "btnExtraAdd";
             this.btnExtraAdd.Size = new System.Drawing.Size(120, 23);
             this.btnExtraAdd.TabIndex = 10;
-            this.btnExtraAdd.Text = "Add Text";
+            this.btnExtraAdd.Text = "Apply Font";
             this.btnExtraAdd.UseVisualStyleBackColor = true;
             this.btnExtraAdd.Click += new System.EventHandler(this.btnExtraAdd_Click);
             // 
-            // btnSave
+            // btnBarcodeFont
             // 
-            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(3, 3);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(894, 94);
-            this.btnSave.TabIndex = 0;
-            this.btnSave.Text = "Save";
-            this.btnSave.UseVisualStyleBackColor = true;
-            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            this.flowLayoutPanel1.SetFlowBreak(this.btnBarcodeFont, true);
+            this.btnBarcodeFont.Location = new System.Drawing.Point(3, 168);
+            this.btnBarcodeFont.Name = "btnBarcodeFont";
+            this.btnBarcodeFont.Size = new System.Drawing.Size(120, 23);
+            this.btnBarcodeFont.TabIndex = 11;
+            this.btnBarcodeFont.Text = "Pick Barcode Font";
+            this.btnBarcodeFont.UseVisualStyleBackColor = true;
+            this.btnBarcodeFont.Click += new System.EventHandler(this.btnBarcodeFont_Click);
+            // 
+            // propertiesLayoutPanel
+            // 
+            this.propertiesLayoutPanel.Controls.Add(this.label2);
+            this.propertiesLayoutPanel.Controls.Add(this.extraTextPropertyTxtBox);
+            this.propertiesLayoutPanel.Controls.Add(this.btnPropertyChangeFont);
+            this.propertiesLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.propertiesLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.propertiesLayoutPanel.Location = new System.Drawing.Point(1039, 3);
+            this.propertiesLayoutPanel.Name = "propertiesLayoutPanel";
+            this.propertiesLayoutPanel.Size = new System.Drawing.Size(173, 639);
+            this.propertiesLayoutPanel.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.label2.Size = new System.Drawing.Size(131, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Text Value";
+            // 
+            // extraTextPropertyTxtBox
+            // 
+            this.extraTextPropertyTxtBox.Location = new System.Drawing.Point(3, 27);
+            this.extraTextPropertyTxtBox.Name = "extraTextPropertyTxtBox";
+            this.extraTextPropertyTxtBox.Size = new System.Drawing.Size(170, 20);
+            this.extraTextPropertyTxtBox.TabIndex = 1;
+            this.extraTextPropertyTxtBox.TextChanged += new System.EventHandler(this.extraTextPropertyTxtBox_TextChanged);
+            // 
+            // btnPropertyChangeFont
+            // 
+            this.btnPropertyChangeFont.Location = new System.Drawing.Point(3, 53);
+            this.btnPropertyChangeFont.Name = "btnPropertyChangeFont";
+            this.btnPropertyChangeFont.Size = new System.Drawing.Size(170, 23);
+            this.btnPropertyChangeFont.TabIndex = 2;
+            this.btnPropertyChangeFont.Text = "Pick Font";
+            this.btnPropertyChangeFont.UseVisualStyleBackColor = true;
+            this.btnPropertyChangeFont.Click += new System.EventHandler(this.btnChangeText_Click);
             // 
             // BarcodeReader
             // 
@@ -195,6 +257,8 @@ namespace QRGenerator
             this.itemListLayout.ResumeLayout(false);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.PerformLayout();
+            this.propertiesLayoutPanel.ResumeLayout(false);
+            this.propertiesLayoutPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -214,5 +278,11 @@ namespace QRGenerator
         private System.Windows.Forms.Button btnExtraAdd;
         private System.Windows.Forms.FontDialog fontPicker;
         private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.FlowLayoutPanel propertiesLayoutPanel;
+        private System.Windows.Forms.Button btnBarcodeFont;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox extraTextPropertyTxtBox;
+        private System.Windows.Forms.Button btnPropertyChangeFont;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
     }
 }
--- QRGenerator/BarcodeReader.cs
+++ QRGenerator/BarcodeReader.cs
@@ -17,7 +17,7 @@ namespace QRGenerator
     {
         private Type Renderer { get; set; }
 
-        //private List<Label> labels = new List<Label>();
+        private List<Label> labels = new List<Label>();
         private Font currentFont;
 
         private bool hasClicked;
@@ -31,9 +31,15 @@ namespace QRGenerator
 
             currentFont = fontPicker.Font;
             Renderer = typeof(BitmapRenderer);
+            RenderLabelPropertyInfo(null);
         }
 
         private void btnBarcodeGenerate_Click(object sender, EventArgs e)
+        {
+            RenderBarcode();
+        }
+
+        private void RenderBarcode()
         {
             try
             {
@@ -45,37 +51,61 @@ namespace QRGenerator
                         Height = barcodePicBox.Height - (barcodePicBox.Height / 4),
                         Width = barcodePicBox.Width
                     },
-                   // Renderer = (IBarcodeRenderer<Bitmap>)Activator.CreateInstance(Renderer)
+                    // Renderer = (IBarcodeRenderer<Bitmap>)Activator.CreateInstance(Renderer)
                     Renderer = new BitmapRenderer //
                     {
-                        TextFont = new Font("Arial", 48, FontStyle.Bold)
+                        TextFont = currentFont
                     }
-
                 };
 
                 barcodePicBox.Image = writer.Write(barcodeTxtBox.Text);
                 barcodePicBox.BackColor = Color.White;
-            }catch(Exception exc)
+            }
+            catch (Exception exc)
             {
                 MessageBox.Show(this, exc.Message, "You fucked up bitch boi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
             }
         }
 
         private void btnExtraAdd_Click(object sender, EventArgs e)
         {
-            Random rnd = new Random();
-            int randomX = rnd.Next(20, this.barcodePicBox.Width - 20);
-            int randomY = rnd.Next(20, this.barcodePicBox.Height - 20);
 
-            var currentLabel = new Label { Text = extraTxtBox.Text, Location = new Point(randomX, randomY), TabIndex = 11};
+            var currentLabel = new Label { Text = extraTxtBox.Text, Location = new Point(0, 0), TabIndex = 11};
+         
             currentLabel.AutoSize = true;
             currentLabel.Font  = currentFont;
             currentLabel.BackColor = System.Drawing.Color.Transparent;
 
+            currentLabel.Location = new Point(this.barcodePicBox.Width - ((this.barcodePicBox.Width / 2) + (currentLabel.Width)),
+                                              this.barcodePicBox.Height - ((this.barcodePicBox.Height / 8) + currentLabel.Height));
+
             currentLabel.MouseDown += lblExtra_MouseDown;
             currentLabel.MouseMove += lblExtra_MouseMove;
-            //labels.Add(currentLabel);
+            labels.Add(currentLabel);
             barcodePicBox.Controls.Add(currentLabel);
+
+            RepositionLabels();
+        }
+
+        private void RepositionLabels()
+        {
+            int labelCount = labels.Count;
+            Point initialReferencePoint = new Point();
+            for(int i = 0; i < labelCount; i++)
+            {
+                var curLabel = labels[i];
+
+                if(i == 0)
+                {
+                    initialReferencePoint = curLabel.Location;
+                }
+                else
+                {
+                    initialReferencePoint = new Point(initialReferencePoint.X, initialReferencePoint.Y - curLabel.Height);
+                    curLabel.Location = initialReferencePoint;
+                }
+            }
         }
 
         private void lblExtra_MouseDown(object sender, MouseEventArgs e)
@@ -89,9 +119,27 @@ namespace QRGenerator
 
             if(e.Button == MouseButtons.Right)
             {
+                labels.Remove(selectedLabel);
                 barcodePicBox.Controls.Remove(selectedLabel);
                 this.selectedLabel = null;
             }
+
+            RenderLabelPropertyInfo(this.selectedLabel);
+        }
+
+        private void RenderLabelPropertyInfo(Label label)
+        {
+            propertiesLayoutPanel.Visible = label != null;
+
+            if (label == null)
+                return;
+
+            extraTextPropertyTxtBox.Text = label.Text;
+        }
+
+        private void ApplyLabelPropertyInfo(Label label)
+        {
+            label.Font = currentFont;
         }
         
         private void lblExtra_MouseMove(object sender, MouseEventArgs e)
@@ -104,6 +152,27 @@ namespace QRGenerator
             }
         }
 
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //TODO: save to specific location using file picker
+            var bitmapImage = new Bitmap(barcodePicBox.ClientRectangle.Width, barcodePicBox.ClientRectangle.Height);
+            barcodePicBox.DrawToBitmap(bitmapImage, new Rectangle(0, 0, bitmapImage.Width, bitmapImage.Height));
+            saveFileDialog.Filter = "PNG Image|*.png;";
+            saveFileDialog.Title = "Save an Image File";
+            saveFileDialog.ShowDialog();
+
+
+            if(saveFileDialog.FileName != "")
+            {
+                System.IO.FileStream fs =
+          (System.IO.FileStream)saveFileDialog.OpenFile();
+                bitmapImage.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
+                fs.Close();
+            }
+            
+        }
+
+        //Font pickers here
         private void btnPickFont_Click(object sender, EventArgs e)
         {
             if (fontPicker.ShowDialog() == DialogResult.OK)
@@ -113,13 +182,32 @@ namespace QRGenerator
             }
         }
 
-        private void btnSave_Click(object sender, EventArgs e)
+        private void btnBarcodeFont_Click(object sender, EventArgs e)
         {
-            //TODO: save to specific location using file picker
-            var bitmapImage = new Bitmap(barcodePicBox.ClientRectangle.Width, barcodePicBox.ClientRectangle.Height);
-            barcodePicBox.DrawToBitmap(bitmapImage, new Rectangle(0, 0, bitmapImage.Width, bitmapImage.Height));
-            bitmapImage.Save("ame_test_png.png", System.Drawing.Imaging.ImageFormat.Png);
+            if(fontPicker.ShowDialog() == DialogResult.OK)
+            {
+                currentFont = fontPicker.Font;
+                btnBarcodeFont.Text = fontPicker.Font.Name;
+                RenderBarcode();
+            }
         }
 
+        private void btnChangeText_Click(object sender, EventArgs e)
+        {
+            if(fontPicker.ShowDialog() == DialogResult.OK)
+            {
+                this.selectedLabel.Font = fontPicker.Font;
+            }
+        }
+
+        private void extraTextPropertyTxtBox_TextChanged(object sender, EventArgs e)
+        {
+            if(this.selectedLabel == null)
+            {
+                return;
+            }
+
+            this.selectedLabel.Text = extraTextPropertyTxtBox.Text;
+        }
     }
 }
